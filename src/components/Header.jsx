15d3d863fed6bef90155cbf8547fa91c1063e638
Arpit Singh <121520688+arpitttttt@users.noreply.github.com>
2025-02-25 05:47:27 +0530

    MultiLanguage Support In Search bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,17 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { auth } from '../utils/firebase';
 import { signOut, onAuthStateChanged } from "firebase/auth";
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addUser, removeUser } from '../utils/userSlice';
-import { LOGO, USERICON } from '../utils/constants';
+import { LOGO, USERICON, SUPPORTED_LANGUAGES } from '../utils/constants';
+import { toggleGptSearchView } from '../utils/gptSlice'
+import { changeLanguage } from '../utils/configSlice';
 
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
+  const showGptSearch= useSelector((store)=>store.gpt.showGptSearch)
 
   // Listen for authentication state changes
   useEffect(() => {
@@ -42,6 +45,14 @@ const Header = () => {
       });
     },[]) 
 
+    const handleGptSearchClick = () => {
+      dispatch(toggleGptSearchView());
+    };
+
+    const handleLanguageChange =(e)=>{
+      dispatch(changeLanguage(e.target.value))
+    }
+
   return (
     <div className='absolute w-screen px-8 py-2 z-10 flex flex-col md:flex-row justify-between'>
       <img
@@ -49,12 +60,28 @@ const Header = () => {
         src={LOGO}
         alt="logo"
       />
-      {user && (  // Only show if user is logged in
-        <div className='flex p-2'>
+      {user && ( 
+        <div className='flex p-2 justify-between'>
+          
+           {showGptSearch && <select
+              className="p-2 m-2 bg-gray-900 text-white"
+              onChange={handleLanguageChange}
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>}
+          
+          <button className='py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg cursor-pointer'
+          onClick={handleGptSearchClick}>
+            {showGptSearch ? "HomePage" : "GPT Search"}
+          </button>
           <img className='w-12 h-12'
            src={USERICON} 
            alt="usericon" />
-          <button onClick={handleSignOut} className="font-bold text-black cursor-pointer">
+          <button onClick={handleSignOut} className="font-bold text-white cursor-pointer">
             (Sign Out)
           </button>
         </div>
